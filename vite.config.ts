e190e460f25d6740e49ae571de53e6b6de9f100c
Hex <hexin@handaotech.com>
2024-03-26 17:06:35 +0800

    init1

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
       vue(),
       Components({
           resolvers: [
-              AntDesignVueResolver({ importStyle: true})
+              AntDesignVueResolver({ importStyle: false})
           ],
       }),
   ],
